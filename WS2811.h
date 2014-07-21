56ffdb179777d17cb1e6d9a3a780b898f11bd53b
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2014-07-21 03:05:55 +0200

    Reorder rgb_t's fields and simplify the assembly loop.
    Reorder RGB to GRB inside the rgb_t struct to match the order the
    hardware accepts.  Since many users of the library will reference
    the fields by name anyway, it is relatively transparent and allows
    us to simplify away 2/3rds of the assembly.

--- WS2811.h
+++ WS2811.h
@@ -1,19 +1,25 @@
 /*
  * Copyright 2012 Alan Burlison, alan@bleaklow.com.  All rights reserved.
  * Use is subject to license terms.
+ *
+ * The assembly rewritten and expanded by Andrew Zaborowski
+ * <andrew.zaborowski@intel.com>.
  */
 
 /*
  * WS2811 RGB LED driver.
+ *
+ * Note that all of the functions below probably read one byte beyond
+ * the end of their input streams.
  */
 
 #ifndef WS2811_h
 #define WS2811_h
 
-// RGB value structure.
+/* RGB value structure reordered for the WS2811's wiring. */
 typedef struct __attribute__ ((__packed__)) {
-    uint8_t r;
     uint8_t g;
+    uint8_t r;
     uint8_t b;
 } RGB_t;
 
@@ -22,135 +28,72 @@ typedef struct __attribute__ ((__packed__)) {
 #endif
 
 /*
- * Inline asm macro to output 24-bit RGB value in (G,R,B) order, MSBit first.
+ * Inline asm macro to output 24-bit GRB value in (G,R,B) order, MSBit first.
  * 0 bits are 250ns hi, 1000ns lo, 1 bits are 1000ns hi, 250ns lo.
- * r0  - saved SREG from outer loop counter decrement
- * r18 - red byte to be output
- * r19 - green byte to be output
- * r20 - blue byte to be output
- * r26 - saved SREG from before interrupt disable
- * r27 - inner loop counter
+ * r18 = byte to be output
+ * r18:r19 = temp value
+ * r16 = saved SREG
+ * r17 = inner loop counter
  */
-#define WS2811(PORT, PIN, RGB, LEN) \
+#define WS2811_OUT_1(PORT, PIN, RGB, LEN) \
 asm volatile( \
 /* initialise */ \
-"    cp %A[len], r1      ; check len > 0, return immediately if it is\n" \
-"    cpc %B[len], r1\n" \
-"    brne 1f\n" \
-"    rjmp 18f\n" \
-"1:  ld r18, Z+           ; load in first red byte to be output\n" \
-"    ld r19, Z+           ; load in first green byte to be output\n" \
-"    ld r20, Z+           ; load in first blue byte to be output\n" \
-"    ldi r27, 8           ; load inner loop counter\n" \
-"    in r26, __SREG__     ; timing-critical, so no interrupts\n" \
+"    movw r18, %A[len]    ; multiply len by 3\n" \
+"    add %A[len], r18\n" \
+"    adc %B[len], r19\n" \
+"    add %A[len], r18\n" \
+"    adc %B[len], r19\n" \
+"    ldi r17, 7           ; load inner loop counter\n" \
+"    in r16, __SREG__     ; timing-critical, so no interrupts\n" \
 "    cli\n" \
-/* green - loop over 8 bits */ \
-"2:  sbi  %[port], %[pin] ; pin lo -> hi\n" \
-"    sbrc r19, 7          ; test hi bit clear\n" \
-"    rjmp 3f              ; true, skip pin hi -> lo\n" \
-"    cbi  %[port], %[pin] ; false, pin hi -> lo\n" \
-"3:  sbrc r19, 7          ; equalise delay of both code paths\n" \
-"    rjmp 4f\n" \
-"4:  nop\n" \
-"    nop\n" \
-"    nop\n" \
-"    nop\n" \
-"    nop\n" \
-"    nop\n" \
-"    lsl r19              ; shift to next bit\n" \
-"    dec r27              ; decrement loop counter\n" \
-"    cbi %[port], %[pin]  ; pin hi -> lo\n" \
-"    brne 2b\n            ; loop if required\n" \
-"    ldi r27, 7           ; reload inner loop counter\n" \
-/* red - loop over first 7 bits */ \
-"5:  sbi %[port], %[pin]  ; pin lo -> hi\n" \
+"    rjmp 6f             ; start with the end-of-loop check\n" \
+/* loop over the first 7 bits */ \
+"1:  sbi  %[port], %[pin] ; pin lo -> hi\n" \
 "    sbrc r18, 7          ; test hi bit clear\n" \
-"    rjmp 6f              ; true, skip pin hi -> lo\n" \
-"    cbi %[port], %[pin]  ; false, pin hi -> lo\n" \
-"6:  sbrc r18, 7          ; equalise delay of both code paths\n" \
-"    rjmp 7f\n" \
-"7:  nop\n" \
+"    rjmp 2f              ; true, skip pin hi -> lo\n" \
+"    cbi  %[port], %[pin] ; false, pin hi -> lo\n" \
+"2:  sbrc r18, 7          ; equalise delay of both code paths\n" \
+"    rjmp 3f\n" \
+"3:  nop                  ; pulse timing delay\n" \
 "    nop\n" \
 "    nop\n" \
 "    nop\n" \
 "    nop\n" \
 "    nop\n" \
 "    lsl r18              ; shift to next bit\n" \
-"    dec r27              ; decrement inner loop counter\n" \
+"    dec r17              ; decrement loop counter, set flags\n" \
 "    cbi %[port], %[pin]  ; pin hi -> lo\n" \
-"    brne 5b              ; inner loop, if required\n" \
-"    nop                  ; equalise delay of both code paths\n" \
-/* red, 8th bit - output & check for end of outer loop */ \
+"    brne 1b              ; (inner) loop if required\n" \
+"    ldi r17, 7           ; reload inner loop counter\n" \
+/* 8th bit - output & fetch next values */ \
 "    sbi %[port], %[pin]  ; pin lo -> hi\n" \
 "    sbrc r18, 7          ; test hi bit clear\n" \
-"    rjmp 8f              ; true, skip pin hi -> lo\n" \
+"    rjmp 4f              ; true, skip pin hi -> lo\n" \
 "    cbi %[port], %[pin]  ; false, pin hi -> lo\n" \
-"8:  sbrc r18, 7          ; equalise delay of both code paths\n" \
-"    rjmp 9f\n" \
-"9:  sbiw %A[len], 1      ; decrement outer loop counter\n" \
-"    in r0, __SREG__      ; save status register\n" \
-"    breq 10f             ; skip if zero\n" \
-"    ld r18, Z+           ; load next red byte\n" \
-"10: brne 11f             ; equalise delay of both code paths\n" \
-"    nop\n" \
-"    nop\n" \
-"11: cbi %[port], %[pin]  ; pin hi -> lo\n" \
-"    ldi r27, 7           ; reload inner loop counter\n" \
-"    nop\n" \
-/* blue - loop over first 7 bits */ \
-"12: sbi %[port], %[pin]  ; pin lo -> hi\n" \
-"    sbrc r20, 7          ; test hi bit clear\n" \
-"    rjmp 13f             ; true, skip pin hi -> lo\n" \
-"    cbi %[port], %[pin]  ; false, pin hi -> lo\n" \
-"13: sbrc r20, 7          ; equalise delay of both code paths\n" \
-"    rjmp 14f\n" \
-"14: nop\n" \
+"4:  sbrc r18, 7          ; equalise delay of both code paths\n" \
+"    rjmp 5f\n" \
+"5:  nop                  ; pulse timing delay\n" \
 "    nop\n" \
 "    nop\n" \
 "    nop\n" \
-"    nop\n" \
-"    nop\n" \
-"    lsl r20              ; shift to next bit\n" \
-"    dec r27              ; decrement inner loop counter\n" \
+"6:  ld r18, %a[rgb]+     ; load next byte\n" \
+"    sbiw %A[len], 1      ; decrement outer loop counter, set flags\n" \
 "    cbi %[port], %[pin]  ; pin hi -> lo\n" \
-"    brne 12b             ; inner loop, if required\n" \
-"    nop                  ; equalise delay of both code paths\n" \
-/* blue, 8th bit -  output & handle outer loop */ \
-"    sbi %[port], %[pin]  ; pin lo -> hi\n" \
-"    sbrc r20, 7          ; test hi bit clear\n" \
-"    rjmp 15f             ; true, skip pin hi -> lo\n" \
-"    cbi %[port], %[pin]  ; false, pin hi -> lo\n" \
-"15: sbrc r20, 7          ; equalise delay of both code paths\n" \
-"    rjmp 16f\n" \
-"16: nop\n" \
-"    ldi r27, 8           ; reload inner loop counter\n" \
-"    out __SREG__, r0     ; restore status register\n" \
-"    breq 17f             ; exit outer loop if zero\n" \
-"    ld r19, Z+           ; load next green byte\n" \
-"    ld r20, Z+           ; load next blue byte\n" \
-"    cbi %[port], %[pin]  ; pin hi -> lo\n" \
-"    rjmp 2b              ; outer loop, if required\n" \
-"17: nop\n" \
-"    nop\n" \
-"    nop\n" \
-"    cbi %[port], %[pin]  ; pin hi -> lo\n" \
-"    nop\n" \
-"    nop\n" \
-"    out __SREG__, r26    ; reenable interrupts\n" \
-"18:\n" \
+"    brge 1b              ; (outer) loop if required \n" \
+"    out __SREG__, r16    ; reenable interrupts if required\n" \
 : \
-: [rgb] "z" (RGB), \
+: [rgb] "e" (RGB), \
   [len] "w" (LEN), \
   [port] "I" (_SFR_IO_ADDR(PORT)), \
   [pin] "I" (PIN) \
-: "r0", "r18", "r19", "r20", "r26", "r27", "cc", "memory" \
+: "r16", "r17", "r18", "r19", "cc", "memory" \
 )
 
 /*
  * Define a C function to wrap the inline WS2811 macro for a given port and pin.
  */
-#define DEFINE_WS2811_FN(NAME, PORT, PIN) \
+#define DEFINE_WS2811_OUT_1_FN(NAME, PORT, PIN) \
 extern void NAME(const RGB_t *rgb, uint16_t len) __attribute__((noinline)); \
-void NAME(const RGB_t *rgb, uint16_t len) { WS2811(PORT, PIN, rgb, len); }
+void NAME(const RGB_t *rgb, uint16_t len) { WS2811_OUT_1(PORT, PIN, rgb, len); }
 
 #endif /* WS2811_h */
